8c4a695c6604577910add0f367f23a765cde2a17
gpe <peterse73@gmail.com>
2018-07-28 00:22:30 +0200

    add domain object

---
+++ spring-boot-docker-domain/src/main/java/nl/gpesoft/sbd/domain/Greeting.java
@@ -0,0 +1,14 @@
+package nl.gpesoft.sbd.domain;
+
+public class Greeting {
+
+    private final String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
